0d10a7e69b795b07c22cd9d9071d196a013a411e
seoyerin <seyerin12@naver.com>
2024-02-11 23:17:15 +0900

    회고, 리포트, 달력에서 사용할 mainHeader 컴포넌트 생성, 회고 입력을 위한 WriteBox 컴포넌트 생성

---
+++ src/components/common/header/MainHeader.jsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export default function MainHeader(props) {
+  return (
+    <Div>
+      <TextBox>{props.children}</TextBox>
+    </Div>
+  );
+}
+
+const Div = styled.div`
+  margin: 44px 0 40px 0;
+  width: 360px;
+  height: 56px;
+`;
+
+const TextBox = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  background-color: #fff;
+
+  .backBtn {
+    width: 45px;
+    height: 45px;
+    font-size: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    left: 24px;
+  }
+  .text {
+    width: 140px;
+    height: 100%;
+    font-size: 16px;
+    font-weight: 600;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .leafIcon {
+    margin-right: 4px;
+    font-size: 22px;
+    color: #5ac479;
+  }
+  .calendarBtn {
+    margin-left: 3px;
+    font-size: 15px;
+    transform: rotate(180deg);
+  }
+`;
---
+++ src/components/retrospection/WriteBox.jsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export default function WriteBox(props) {
+  return (
+    <>
+      <Title>{props.children}</Title>
+      <TextArea maxLength={600}></TextArea>
+    </>
+  );
+}
+
+const Title = styled.div`
+  margin-bottom: 9px;
+  width: 312px;
+  height: 25px;
+  font-size: 14px;
+  font-weight: 600;
+  display: flex;
+  align-items: center;
+
+  .icon {
+    width: 24px;
+    height: 24px;
+    color: #5ac479;
+  }
+`;
+const TextArea = styled.textarea`
+  padding: 5px;
+  margin-bottom: 20px;
+  width: 302px;
+  height: 112px;
+  border: none;
+  border-radius: 5px;
+  font-size: 12px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+  resize: none;
+  outline: none;
+  background: #fff;
+`;
