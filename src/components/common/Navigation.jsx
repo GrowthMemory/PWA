b1f8e152d68d84ae9ae0b9e4655b93a3dceee826
seoyerin <seyerin12@naver.com>
2024-02-13 00:23:39 +0900

    하단 navigation에 라우터 연결

--- src/components/common/Navigation.jsx
+++ src/components/common/Navigation.jsx
@@ -4,15 +4,22 @@ import { PiChartLine } from "react-icons/pi";
 import { FaCalendar } from "react-icons/fa";
 import { BsChatFill } from "react-icons/bs";
 import { IoMdSettings } from "react-icons/io";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Navigation() {
   const location = useLocation().pathname;
+  const navigate = useNavigate();
   return (
     <Div>
-      <MdHome style={location == "/Home" ? { color: "#5ac479" } : ""} />
+      <MdHome
+        style={location == "/Home" ? { color: "#5ac479" } : ""}
+        onClick={() => navigate("/Home")}
+      />
       <PiChartLine style={location == "/Report" ? { color: "#5ac479" } : ""} />
-      <FaCalendar style={location == "/Calendar" ? { color: "#5ac479" } : ""} />
+      <FaCalendar
+        style={location == "/Calendar" ? { color: "#5ac479" } : ""}
+        onClick={() => navigate("/Calendar")}
+      />
       <BsChatFill style={location == "/Chat" ? { color: "#5ac479" } : ""} />
       <IoMdSettings style={location == "/MyPage" ? { color: "#5ac479" } : ""} />
     </Div>
