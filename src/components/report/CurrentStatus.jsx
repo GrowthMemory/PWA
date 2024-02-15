49042e226a99f4ae816307cab91d234596359781
seoyerin <seyerin12@naver.com>
2024-02-15 23:21:07 +0900

    현재 상태 게이지 바 state 연결 alc ㅊㄴㄴ

--- src/components/report/CurrentStatus.jsx
+++ src/components/report/CurrentStatus.jsx
@@ -1,10 +1,26 @@
+import { useContext } from "react";
 import styled from "styled-components";
+import { ReportContext } from "../context/context";
 
 export default function CurrentStatus() {
+  const { currentStatusObj, setCurrentStatusObj } = useContext(ReportContext);
   return (
     <Div>
       <Title>현재 나의 상태는 이래요!</Title>
-      <Status>status</Status>
+      <Status>
+        <Input
+          type="range"
+          id="volume"
+          name="volume"
+          min="0"
+          max="100"
+          defaultValue={currentStatusObj.score}
+          readOnly
+        />
+        <p>
+          {`평균 ${currentStatusObj.score}점 입니다. ${currentStatusObj.text} 상태에요`}
+        </p>
+      </Status>
     </Div>
   );
 }
@@ -26,6 +42,38 @@ const Status = styled.div`
   width: 100%;
   height: 85px;
   border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
   background: #fff;
   box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.3);
+`;
+
+const Input = styled.input`
+  width: 288px;
+  height: 20px;
+  -webkit-appearance: none;
+
+  &::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 16px;
+    border-radius: 20px;
+    background: linear-gradient(
+      270deg,
+      #29e05f 1.41%,
+      rgba(121, 255, 160, 0.16) 99.98%
+    );
+  }
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    width: 24px;
+    height: 24px;
+    border-radius: 100px;
+    border: 4px solid var(--primary, #5ac479);
+    position: relative;
+    bottom: 4px;
+    background: #fff;
+    box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.1);
+  }
 `;
