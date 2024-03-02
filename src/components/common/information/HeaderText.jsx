cff44011053972f5d1c2264b28c2d1d63bd63e48
seoyerin <seyerin12@naver.com>
2024-03-03 04:22:05 +0900

    약관 텍스트 추가

--- src/components/common/information/HeaderText.jsx
+++ src/components/common/information/HeaderText.jsx
@@ -1,10 +1,16 @@
 import { INFORMATIONTITLE } from "../key";
 import * as s from "../../css/common/information";
+import { useNavigate } from "react-router-dom";
 export default function HeaderText() {
   let informationTitle = localStorage.getItem(INFORMATIONTITLE) ?? "";
+  const navigate = useNavigate();
   return (
     <>
-      <s.BackBtn />
+      <s.BackBtn
+        onClick={() => {
+          navigate(-1);
+        }}
+      />
       <span>{informationTitle}</span>
     </>
   );
