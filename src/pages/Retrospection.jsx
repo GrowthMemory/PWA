8e6c86fb98f222e6d443700621e884ad3697c566
seoyerin <seyerin12@naver.com>
2024-03-04 02:38:57 +0900

    회고 데이터 반영 안내 추가

--- src/pages/Retrospection.jsx
+++ src/pages/Retrospection.jsx
@@ -1,70 +1,44 @@
-import styled from "styled-components";
 import WriteProvider from "../components/provider/WriteProvider";
 import MainHeader from "../components/common/header/MainHeader";
 import WriteBox from "../components/retrospection/WriteBox";
 import HeaderText from "../components/retrospection/HeaderText";
-import { HiLocationMarker } from "react-icons/hi";
-import { TiWarning } from "react-icons/ti";
 import ReportProvider from "../components/provider/ReportProvider";
 import ReportCalendar from "../components/common/InputCalendar";
+import NoticeModal from "../components/retrospection/NoticeModal";
+import * as s from "../components/css/retrospection/retrospection";
 import { useState } from "react";
 
 export default function Retrospection() {
   const [showCalendar, setShowCalendar] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   return (
-    <RetrospectionBox>
+    <s.RetrospectionBox>
       <WriteProvider>
         <ReportProvider>
           <MainHeader>
-            <HeaderText setShowCalendar={setShowCalendar} />
+            <HeaderText
+              setShowCalendar={setShowCalendar}
+              setShowModal={setShowModal}
+            />
           </MainHeader>
           {showCalendar && <ReportCalendar setShowCalendar={setShowCalendar} />}
+          {showModal && <NoticeModal setShowModal={setShowModal} />}
         </ReportProvider>
-        <Div>
+        <s.Div>
           <WriteBox id={"keep"}>
-            <KeepIcon />
+            <s.KeepIcon />
             <span>현재까지 만족한 점: KEEP</span>
           </WriteBox>
           <WriteBox id={"problem"}>
-            <ProblemIcon />
+            <s.ProblemIcon />
             <span>아쉬웠거나 개선이 필요하다고 느낀 점: Problem</span>
           </WriteBox>
           <WriteBox id={"try"}>
-            <TryIcon src="img/sprout.png" alt="" />
+            <s.TryIcon src="img/sprout.png" alt="" />
             <span>앞으로 개선할 점: Try</span>
           </WriteBox>
-        </Div>
+        </s.Div>
       </WriteProvider>
-    </RetrospectionBox>
+    </s.RetrospectionBox>
   );
 }
-
-const RetrospectionBox = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-`;
-
-const Div = styled.div`
-  width: 360px;
-  height: 490px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const KeepIcon = styled(HiLocationMarker)`
-  font-size: 24px;
-  color: #5ac479;
-`;
-
-const ProblemIcon = styled(TiWarning)`
-  font-size: 24px;
-  color: #5ac479;
-`;
-const TryIcon = styled.img`
-  width: 24px;
-  height: 24px;
-  color: #5ac479;
-`;
